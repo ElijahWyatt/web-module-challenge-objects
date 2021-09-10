542244c5545ac4aa7823fab794ad761f8b7a682f
unknown <elijah.wyatt3@gmail.com>
2021-09-09 22:59:37 -0500

    solved task 2

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ function createMenuItem(name, price, category){
     return {name, price, category};
 }
 
+
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1b: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Invoke your function!
 Test your createMenuItems function by doing the following:
@@ -28,6 +29,9 @@ Test your createMenuItems function by doing the following:
   
   For example: createMenuItem("pizza",5,"lunch") would return this as the object: {name:"Pizza",price:5,category:"lunch"}
 */
+console.log(createMenuItem("pizza", 5, "Lunch"));
+console.log(createMenuItem("biscuits & gravy", 6, "Breakfast"));
+console.log(createMenuItem("steak & potatoes", 17, "Dinner"));
 
 
 
@@ -47,8 +51,18 @@ const burger = {
   name: "Burger", 
   price: 18, 
   category: "Lunch", 
-  
+  discount: function(person){
+    if(person === 'teacher' || person === 'student'){
+      return this.price - (this.price * 0.25);
+    }else if(person === 'public'){
+      return this.price - (this.price * 0.10);
+    }else{
+      return 'NO DISCOUNT';
+    }
+  }
 }
+
+console.log(burger.discount('teacher'));
 
 
 
